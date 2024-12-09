a26201dcff0801818c2394bce590eac0ebf5cbfc
AltynbekovAskat <AltynbekovAskat@gmail.com>
2024-12-09 16:28:38 +0600

    Models

--- mysite/Course/models.py
+++ mysite/Course/models.py
@@ -1,3 +1,139 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_name = models.CharField(max_length=32, unique=True)
+    ROLE_CHOICES = [
+        ('студент', 'студент'),
+        ('препадватель', 'преподаватель'),
+        ('администратор', 'администратор'),
+]
+    role = models.CharField(max_length=40, choices=ROLE_CHOICES, default='клиент')
+
+    def __str__(self):
+        return f'{self.user_name}'
+
+
+class Teacher(models.Model):
+    teacher_name = models.CharField(max_length=57)
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='picture/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_owner')
+
+    def __str__(self):
+        return f'{self.teacher_name}, {self.owner}'
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return f'{self.category_name}'
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    LEVEL_CHOICES = [
+        ('начальный', 'начальный'),
+        ('cредний', 'cредний'),
+        ('продвинутый', 'продвинутый')
+    ]
+    level = models.CharField(max_length=55, choices=LEVEL_CHOICES)
+    price = models.PositiveIntegerField(null=True, blank=True)
+    created_by = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    create_at = models. DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.course_name}, {self.category}'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=32)
+    video_url = models.FileField(upload_to='video/', null=True, blank=True)
+    content = models.TextField()
+    course = models.ForeignKey(Course,on_delete=models.CASCADE, related_name='course_lesson')
+
+    def __str__(self):
+        return f'{self.title}, {self.course}'
+
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=32)
+    student_bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile_picture/')
+    student_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='student_owner')
+
+    def __str__(self):
+        return f'{self.student_bio}, {self.student_owner}'
+
+
+class Assignment(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+    due_date = models.DateTimeField(auto_now_add=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_assignment')
+    students = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='assignment_student')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='lesson_assignment')
+
+    def __str__(self):
+        return f'{self.title}, {self.course}'
+
+
+class Questions(models.Model):
+    questions_name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f'{self.questions_name}'
+
+
+class Exam(models.Model):
+    title = models.CharField(max_length=32)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='exam')
+    questions = models.ForeignKey(Questions, on_delete=models.CASCADE, related_name='questions')
+    passing_score = models.IntegerField(choices=[(i, str(i)) for i in range(1, 101)], verbose_name='Баллы')
+    duration = models.DateField()
+
+    def __str__(self):
+        return f'{self.title}, {self.questions}'
+
+
+class Certificate(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='student')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_certificate')
+    issued_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    certificate = models.CharField(max_length=32)
+    certificate_url = models.FileField(upload_to='certificate_video/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.student}, {self.certificate}'
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+
+    def __str__(self):
+        return f'{self.user}'
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveSmallIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.course}, {self.quantity}'
+
+
+class Review(models.Model):
+    user = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='user')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='course_review')
+    instructor = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='teacher_review')
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    comment = models.TextField()
+
+    def __str__(self):
+        return f'{self.user}, {self.course}'
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,28 +21,34 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hh!4k37x3p9f9u)0-_b)j=2i*yz3i_43z52%$2t3#)11gb^+04'
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'modeltranslation',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Course',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -103,21 +110,32 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
-
+LANGUAGES = (
+    ('en', 'English'),
+    ('ru', 'Russian'),
+)
+MODELTRANSLATION_DEFAULT_LANGUAGE = 'en'
+MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'Course.User'
